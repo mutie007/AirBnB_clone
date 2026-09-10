e1bf915ac23f55c3237352181819750fe0555741
mutie007 <your@email.com>
2026-09-10 20:38:49 +0300

    Add FileStorage and link BaseModel to storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """This module defines the BaseModel class."""
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -26,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            from models import storage
             storage.new(self)
 
     def __str__(self):
@@ -34,16 +34,13 @@ class BaseModel:
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the public instance attribute updated_at
-        with the current datetime and save to storage.
-        """
+        """Update updated_at and save to storage."""
         self.updated_at = datetime.now()
+        from models import storage
         storage.save()
 
     def to_dict(self):
-        """Return a dictionary containing all keys/values of __dict__
-        of the instance.
-        """
+        """Return a dictionary containing all keys/values of the instance."""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,8 +35,6 @@ class FileStorage:
             with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
             for key, value in obj_dict.items():
-                class_name = value["__class__"]
-                if class_name == "BaseModel":
-                    FileStorage.__objects[key] = BaseModel(**value)
+                FileStorage.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
             pass
