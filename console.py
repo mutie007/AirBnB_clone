68983086abe7db657ddc451f1029890bdf26c3b7
mutie007 <your@email.com>
2026-09-15 20:40:27 +0300

    Add console 0.1 with create, show, destroy, all, update

--- console.py
+++ console.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """Command interpreter for the AirBnB clone project."""
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class."""
 
     prompt = "(hbnb) "
+    classes = {
+        "BaseModel": BaseModel
+    }
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -20,6 +25,114 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty line."""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel, save it and print the id."""
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in self.classes:
+            print("** class doesn't exist **")
+            return
+        obj = self.classes[arg]()
+        obj.save()
+        print(obj.id)
+
+    def do_show(self, arg):
+        """Print the string representation of an instance."""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        print(storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Delete an instance based on the class name and id."""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        del storage.all()[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Print all string representation of all instances."""
+        args = arg.split()
+        obj_list = []
+        if len(args) == 0:
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+        else:
+            if args[0] not in self.classes:
+                print("** class doesn't exist **")
+                return
+            for key, obj in storage.all().items():
+                if key.startswith(args[0] + "."):
+                    obj_list.append(str(obj))
+        print(obj_list)
+
+    def do_update(self, arg):
+        """Update an instance based on the class name and id."""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args) == 3:
+            print("** value missing **")
+            return
+
+        obj = storage.all()[key]
+        attr_name = args[2]
+        attr_value = args[3]
+
+        # Remove quotes if present
+        if attr_value.startswith('"') and attr_value.endswith('"'):
+            attr_value = attr_value[1:-1]
+        else:
+            # Try to cast to int or float
+            try:
+                if '.' in attr_value:
+                    attr_value = float(attr_value)
+                else:
+                    attr_value = int(attr_value)
+            except ValueError:
+                pass
+
+        setattr(obj, attr_name, attr_value)
+        obj.save()
 
 
 if __name__ == '__main__':
